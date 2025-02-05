9221f2f1c5fb92e88b63749d59af808ff8a6f85d
Jhonny Lima <lima.jhonny95@gmail.com>
2025-02-04 21:27:10 -0300

    feat: create QuestionPresenter

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { QuestionPresenter } from '../presenters/question-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -24,11 +25,15 @@ export class FetchRecentQuestionsController {
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const questions = await this.fetchRecentQuestionsUseCase.execute({
+    const result = await this.fetchRecentQuestionsUseCase.execute({
       page,
     })
+    if (result.isLeft()) {
+      throw new Error()
+    }
+    const questions = result.value.questions
     return {
-      questions,
+      questions: questions.map(QuestionPresenter.toHttp),
     }
   }
 }
