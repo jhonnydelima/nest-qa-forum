1e6c9f286d896e10354f090e9bf994067677591e
Jhonny Lima <lima.jhonny95@gmail.com>
2025-02-14 21:31:23 -0300

    feat: add `delete-answer.controller`

---
+++ src/infra/http/controllers/delete-answer.controller.ts
@@ -0,0 +1,31 @@
+import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+} from '@nestjs/common'
+
+@Controller('/answers/:id')
+export class DeleteAnswerController {
+  constructor(private readonly deleteAnswerUseCase: DeleteAnswerUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') answerId: string,
+  ) {
+    const userId = user.sub
+    const result = await this.deleteAnswerUseCase.execute({
+      answerId,
+      authorId: userId,
+    })
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
