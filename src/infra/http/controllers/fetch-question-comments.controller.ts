2998eabd1f003bf18dd8397f463b8c7eabb12f2c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-02-16 20:12:28 -0300

    feat: add `fetch-question-comments.controller`

---
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions/:questionId/comments')
+export class FetchQuestionCommentsController {
+  constructor(
+    private readonly fetchQuestionCommentsUseCase: FetchQuestionCommentsUseCase,
+  ) {}
+
+  @Get()
+  async handle(
+    @Query('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionCommentsUseCase.execute({
+      questionId,
+      page,
+    })
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+    const questionComments = result.value.questionComments
+    return {
+      comments: questionComments.map(CommentPresenter.toHttp),
+    }
+  }
+}
