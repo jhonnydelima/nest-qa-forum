f55d3071b2f842e2d703aa704ec829a847d02db9
Jhonny Lima <lima.jhonny95@gmail.com>
2025-02-17 22:25:32 -0300

    feat: add `prisma-attachments-repository`

---
+++ src/infra/database/prisma/mappers/prisma-attachment-mapper.ts
@@ -0,0 +1,22 @@
+import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
+import { Prisma, Attachment as PrismaAttachment } from '@prisma/client'
+
+export class PrismaAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): Attachment {
+    return Attachment.create(
+      {
+        title: raw.title,
+        url: raw.url,
+      },
+      raw.id,
+    )
+  }
+
+  static toPrisma(attachment: Attachment): Prisma.AttachmentUncheckedCreateInput {
+    return {
+      id: attachment.id.toString(),
+      title: attachment.title,
+      url: attachment.url,
+    }
+  }
+}
