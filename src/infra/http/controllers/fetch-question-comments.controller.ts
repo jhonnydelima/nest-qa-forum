cd8f1590f26918c0930ffccc34c43aca168e5b2f
Jhonny Lima <lima.jhonny95@gmail.com>
2025-02-28 23:05:25 -0300

    refactor: update `fetch-question-comments.controller`

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -8,7 +8,7 @@ import {
 import { z } from 'zod'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
-import { CommentPresenter } from '../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -39,9 +39,9 @@ export class FetchQuestionCommentsController {
     if (result.isLeft()) {
       throw new BadRequestException()
     }
-    const questionComments = result.value.questionComments
+    const comments = result.value.comments
     return {
-      comments: questionComments.map(CommentPresenter.toHttp),
+      comments: comments.map(CommentWithAuthorPresenter.toHttp),
     }
   }
 }
