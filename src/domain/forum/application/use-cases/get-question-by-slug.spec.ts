0613945cd48d16bfed98f7c29859e3803297ef8c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-03-03 22:56:43 -0300

    test: refactor `get-question-by-slug` tests

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -3,31 +3,58 @@ import { GetQuestionBySlugUseCase } from './get-question-by-slug'
 import { makeQuestion } from 'test/factories/make-question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
 import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository'
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments-repository'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { makeStudent } from 'test/factories/make-student'
+import { makeAttachment } from 'test/factories/make-attachment'
+import { makeQuestionAttachment } from 'test/factories/make-question-attachment'
 
 let questionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
+let attachmentsRepository: InMemoryAttachmentsRepository
+let studentsRepository: InMemoryStudentsRepository
 let questionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
 
 describe('Get Question By Slug Use Case', () => {
   beforeEach(() => {
     questionAttachmentsRepository = new InMemoryQuestionAttachmentsRepository()
+    attachmentsRepository = new InMemoryAttachmentsRepository()
+    studentsRepository = new InMemoryStudentsRepository()
     questionsRepository = new InMemoryQuestionsRepository(
       questionAttachmentsRepository,
+      attachmentsRepository,
+      studentsRepository,
     )
     sut = new GetQuestionBySlugUseCase(questionsRepository)
   })
 
   it('should be able to get a question by slug', async () => {
+    const student = makeStudent({ name: 'John Doe' })
+    await studentsRepository.create(student)
     const newQuestion = makeQuestion({
       slug: Slug.create('example-slug'),
+      authorId: student.id,
     })
     await questionsRepository.create(newQuestion)
+    const attachment = makeAttachment({ title: 'New attachment' })
+    await attachmentsRepository.create(attachment)
+    await questionAttachmentsRepository.createMany([
+      makeQuestionAttachment({
+        attachmentId: attachment.id,
+        questionId: newQuestion.id,
+      }),
+    ])
     const result = await sut.execute({ slug: 'example-slug' })
     expect(result.isRight()).toBe(true)
     expect(result.value).toMatchObject({
       question: expect.objectContaining({
-        id: newQuestion.id,
         title: newQuestion.title,
+        author: 'John Doe',
+        attachments: [
+          expect.objectContaining({
+            title: 'New attachment',
+          }),
+        ],
       }),
     })
   })
